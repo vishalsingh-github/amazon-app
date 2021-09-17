9b5139262c1e1d2dbd4d22ee533c140c4a50338c
vishalsingh-github <thenameisvishal2311@gmail.com>
2021-09-17 13:43:53 +0530

    redux store

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Product from "../component/Product";
-import axios from "axios";
 import LoadingBox from "../component/LoadingBox";
 import MessageBox from "../component/MessageBox";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState("")
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
-    const fetchData = async () => {
-        try{
-        setLoading(true)
-        const { data } = await axios.get("/api/products");
-        setLoading(false);
-        setProducts(data);
-      }
-      catch(err){
-        setError(err.message)
-        setLoading(false)
-      };
-      }
-    fetchData();
+    dispatch(listProducts());
   }, []);
   return (
     <div>
