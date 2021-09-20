d7d107776f6c611911631472469b698338650f6d
vishalsingh-github <thenameisvishal2311@gmail.com>
2021-09-20 11:23:06 +0530

    Cart complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,33 @@
-
-import {BrowserRouter, Route} from "react-router-dom"
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               amazona
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">Cart</Link>
+            {cartItems.length > 0 && (
+              <span className="badge">{cartItems.length}</span>
+            )}
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
-          <Route path="/product/:id" component={ProductScreen}/>
-          <Route exact path="/" component={HomeScreen}/>
-
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route exact path="/" component={HomeScreen} />
         </main>
         <footer className="row center">All right reserved</footer>
       </div>
